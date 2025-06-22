a7f93d2d1818592a582a077257b91706185d2a45
moham <mohamurshid153@gmail.com>
2025-06-22 08:11:05 +0300

    Changed logic of marketplace such that everyone can view it

--- public/marketplace.php
+++ public/marketplace.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Verify buyer is logged in
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'buyer') {
+if (!isset($_SESSION['user_id'])) {
     header("Location: ../public/login.php");
     exit();
 }
