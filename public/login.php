218f0aefde032247b149a5f0d814dcaf1090d861
moham <mohamurshid153@gmail.com>
2025-07-03 08:50:08 +0300

    changed things in the navbar for buyer_cart, login and products

--- public/login.php
+++ public/login.php
@@ -31,7 +31,7 @@ if ($conn->connect_error) {
         <ul class="nav-links">
             <li><a href="index.php">Home</a></li>
             <li><a href="products.php">Products</a></li>
-            <li><a href="services.php">Services</a></li>
+            <li><a href="../public/services_home.php">Services</a></li>
             <li><a href="login.php" class="login-btn">Login</a></li>
         </ul>
     </nav>
